97c282c06165f98515cc87df910757981e8842c1
JVGS1 <50575899+JVGS1@users.noreply.github.com>
2021-02-17 22:28:49 +0000

    added detection for gestures

--- src/App.js
+++ src/App.js
@@ -1,64 +1,68 @@
 import './App.css';
-import React, { useRef, useState } from 'react';
+import React, { useRef, useState, useEffect} from 'react';
 import * as tensorflow from "@tensorflow/tfjs";
 import * as handpose from "@tensorflow-models/handpose"
 import Webcam from "react-webcam"
-import { detectHand } from './components/getGesture'
+import { DetectHand } from './components/getGesture'
 
 //Import FP
 import victory from './victory.png'
 import thumbs_up from './thumbs_up.png'
 
 
+// this.state = useState({})
+
+
+
 function App() {
   const webcamRef = useRef(null);
-  const canvasRef = useRef(null)
+  const canvasRef = useRef(null);
+  const [currentGesture, setGesture] = useState(null);
+  const image = {thumbs_up: thumbs_up, victory: victory};
 
-  const runHandPose = async () => {
-    const net = await handpose.load()
-    console.log('Model Loaded');
+  // console.log(webcamRef)
 
-    if (webcamRef) {
-      setInterval(() => {
-        detectHand(net, canvasRef, webcamRef)
-      }, 100)
-    };
+
+  runHandPose(webcamRef, canvasRef);
+
+
+  const styling = {
+    position: "absolute",
+    marginLeft: "auto",
+    marginRight: "auto",
+    left: "0",
+    right: "0",
+    textAlign: "center",
+    zIndex: "9",
+    width: "640",
+    height: "480"
   }
-  runHandPose();
+
 
   return (
     <div className="App">
       <header className="App-header">
 
         <Webcam ref={webcamRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: "0",
-            right: "0",
-            textAlign: "center",
-            zIndex: "9",
-            width: "640",
-            height: "480"
-          }} />
+          style={styling} />
 
         <canvas ref={canvasRef}
-          style={{
-            position: "absolute",
-            marginLeft: "auto",
-            marginRight: "auto",
-            left: "0",
-            right: "0",
-            textAlign: "center",
-            zIndex: "9",
-            width: "640",
-            height: "480"
-          }}
+          style={styling}
         />
       </header>
     </div>
   );
+}
+
+
+const runHandPose = async (webcamRef, canvasRef) => {
+  const net = await handpose.load();
+  console.log('Model Loaded');
+  if (webcamRef) {
+    setInterval(() => {
+       this.x = DetectHand(net, canvasRef, webcamRef)
+    }, 100)
+  };
 }
 
 export default App;
--- src/components/getGesture.js
+++ src/components/getGesture.js
@@ -1,23 +1,32 @@
 import React, { useRef, useState } from 'react';
 import * as fp from 'fingerpose'
 import { drawHand } from './canvasDraw'
+import { webcam } from '@tensorflow/tfjs-data';
 
-export const detectHand = async (net, canvasRef, webcamRef) => {
+import victory from '../victory.png'
+import thumbs_up from '../thumbs_up.png'
 
-    if (typeof webcamRef.current !== "undefined" &&
-        webcamRef.current !== null &&
-        webcamRef.current.video.readyState === 4) {
-        const video = webcamRef.current.video;
-        const videoWidth = webcamRef.current.video.videoWidth;
-        const videoHeight = webcamRef.current.video.videoHeight;
+export const DetectHand = async (net, ref_canvas, ref_webcam) => {
 
 
-        webcamRef.current.video.width = videoWidth;
-        webcamRef.current.video.height = videoHeight;
+
+    // console.log(ref_webcam)
+    
+
+    if (typeof ref_webcam.current !== "undefined" &&
+        ref_webcam.current !== null &&
+        ref_webcam.current.video.readyState === 4) {
+        const video = ref_webcam.current.video;
+        const videoWidth = ref_webcam.current.video.videoWidth;
+        const videoHeight = ref_webcam.current.video.videoHeight;
 
 
-        canvasRef.current.width = videoWidth;
-        canvasRef.current.height = videoHeight
+        ref_webcam.current.video.width = videoWidth;
+        ref_webcam.current.video.height = videoHeight;
+
+
+        ref_canvas.current.width = videoWidth;
+        ref_canvas.current.height = videoHeight
 
 
         //Make detections
@@ -34,13 +43,27 @@ export const detectHand = async (net, canvasRef, webcamRef) => {
             ])
 
             //Waiting for gestures to return and using a confidence score of 8 
-            const gesture = await GE.estimate(hand[0].landmarks, 8)
-            console.log(gesture);
+            const gesture = await GE.estimate(hand[0].landmarks, 8);
+
+
+
+            //retrieves gesture coressponding to highest confidence score
+            if(gesture.gestures !== undefined && gesture.gestures.length > 0 ){
+
+                return gesture;
+                // const confidenceScore = gesture.gestures.map((accuracy) => accuracy.confidence);
+                // console.log(Math.max.apply(null, confidenceScore))
+                // //returning index 
+                // const highestScore = confidenceScore.indexOf(Math.max.apply(null, confidenceScore));
+                // console.log(gesture.gestures[highestScore].name)
+                // // setGesture(gesture.gestures[highestScore].name);
+                // // console.log(currentGesture);
+            }
 
         }
 
 
-        const canvas = canvasRef.current.getContext("2d");
+        const canvas = ref_canvas.current.getContext("2d");
         drawHand(hand, canvas)
     }
 
